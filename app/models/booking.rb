98bb00ea5f40e67648dff22dcbc2e83b7c32da9d
Nominoe GERON <nominoe.geron@gmail.com>
2022-02-22 15:31:09 +0100

    Seed created and controller misstakes corrected

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,27 @@
 class Booking < ApplicationRecord
-  belongs_to :users
-  belongs_to :costumes
+  belongs_to :user
+  belongs_to :costume
   validates :status, :start_date, :end_date, presence: true
-  validates :start_date, comparison: { greater_than: :end_date }
   validates :status, inclusion: { in: ["pending", "accepted", "cancelled"] }
+  validate :start_date_future?
+  validate :end_date_after_start_date
+
+  private
+
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
+
+  def start_date_future?
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
 end
