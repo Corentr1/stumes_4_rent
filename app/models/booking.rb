cb6af6422626fdee61bbb24f5908f7bde5674b15
Corentin Peyresblanques <corentin.peyresblanques@gmail.com>
2022-02-22 11:25:43 +0100

    forgot to save my changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  has_many :users
-  has_many :costumes
+  belongs_to :users
+  belongs_to :costumes
   validates :status, :start_date, :end_date, presence: true
   validates :start_date, comparison: { greater_than: :end_date }
   validates :status, inclusion: { in: ["pending", "accepted", "cancelled"] }
