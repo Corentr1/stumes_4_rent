7882d2d2e1912e3b13ac82eb6ef5fc8a3b0843dd
Nominoe GERON <nominoe.geron@gmail.com>
2022-02-22 12:08:05 +0100

    Models work

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -8,11 +8,12 @@ class CostumesController < ApplicationController
   end
 
   def show
-    @costume = costume.find(params[:id])
+    @costume = Costume.find(params[:id])
   end
 
   def create
     @costume = Costume.new(costume_params)
+    @costume.user = current_user
     if @costume.save
       redirect_to costume_path(@costume)
     else
@@ -23,6 +24,6 @@ class CostumesController < ApplicationController
   private
 
   def costume_params
-    params.require(:costume).permit(:name, :category, :price_per_day, :available?, :image_url)
+    params.require(:costume).permit(:name, :category, :size, :price_per_day, :available?, :image_url)
   end
 end
