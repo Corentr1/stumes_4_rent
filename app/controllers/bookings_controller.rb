b8b88a47b8ea522edcceed6d46a21557f7fd9525
Nominoe GERON <nominoe.geron@gmail.com>
2022-02-23 10:52:15 +0100

    Starting again booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,20 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @costume = Costume.find(params[:costume_id])
     @booking = Booking.new
   end
+
+  def create
+    @bookming = Booking.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+
 end
